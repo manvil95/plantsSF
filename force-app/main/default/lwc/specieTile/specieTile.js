6c5acc8a6a6caa676077dd915ce52aa6e2467e31
Manuel Villen <manuvillen@gmail.com>
2023-12-28 18:49:14 +0100

    Tab edit

--- force-app/main/default/lwc/specieTile/specieTile.js
+++ force-app/main/default/lwc/specieTile/specieTile.js
@@ -5,12 +5,12 @@ import FORM_FACTOR from "@salesforce/client/formFactor";
 
 export default class SpecieTile extends NavigationMixin(LightningElement) {
   /*specie = {
-    Name: "Jazmin",
-    Description: "Olorosa y bonita planta trepadora",
-    Image_URL__c:
-      "https://i.pinimg.com/originals/88/a4/9f/88a49f73cb34bb49ea799087ad2fba15.jpg",
-    Location__c: "Indoors,Outdoors"
-  };*/
+		Name: "Jazmin",
+		Description: "Olorosa y bonita planta trepadora",
+		Image_URL__c:
+			"https://i.pinimg.com/originals/88/a4/9f/88a49f73cb34bb49ea799087ad2fba15.jpg",
+		Location__c: "Indoors,Outdoors"
+	};*/
   @api specie;
 
   get imageURLFromStaticResource() {
